e7fef63b7493bba962c33afff5187b8385a1b22e
Pruthviraj Mane <61158795+pruthvi54@users.noreply.github.com>
2021-09-15 22:16:06 +0530

    Add files via upload

---
+++ a_cruise_control.m
@@ -0,0 +1,11 @@
+% Linearized vehicle parameters
+K = 0.07536;
+t= 75.36;
+%Additional vehicle parameters 
+Mv= 1000; % in kg
+g= 9.8 ; 
+rw = 0.3 ; % wheel radius in m
+
+% ACC parameters
+ddefault = 5 ; % in meters
+tlag = 3 ; % in sec > reaction time
